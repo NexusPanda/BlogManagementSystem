5bb97ae670fd044e2db79f20a909a447cd03da90
NexusPanda <157189888+NexusPanda@users.noreply.github.com>
2025-06-09 15:16:58 +0530

    Added Additional Dependency

---
+++ BlogManagementSystem/src/main/java/com/Blog/BlogManagementSystem/Model/User.java
@@ -0,0 +1,30 @@
+package com.Blog.BlogManagementSystem.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+}
